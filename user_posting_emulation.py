2defb12f70e1e8e7e447a179ae65f0a96b3d6818
AlanKawaHF <70645103+AlanKawaHF@users.noreply.github.com>
2024-09-03 16:38:54 +0100

    Script

---
+++ user_posting_emulation.py
@@ -0,0 +1,85 @@
+import requests
+from time import sleep
+import random
+from multiprocessing import Process
+import boto3
+import json
+import sqlalchemy
+from sqlalchemy import text
+import yaml
+
+
+random.seed(100)
+
+
+class AWSDBConnector:
+
+    def __init__(self):
+
+        self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
+        self.USER = 'project_user'
+        self.PASSWORD = ':t%;yCY3Yjg'
+        self.DATABASE = 'pinterest_data'
+        self.PORT = 3306
+        
+    def create_db_connector(self):
+        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        return engine
+
+
+new_connector = AWSDBConnector()
+
+API_URL = "https://jdneb8yyrb.execute-api.us-east-1.amazonaws.com/prod"
+user_topic_endpoint = f"{API_URL}/user-topic"
+pin_topic_endpoint = f"{API_URL}/pin-topic"
+geo_topic_endpoint = f"{API_URL}/geo-topic"
+
+def run_infinite_post_data_loop():
+    while True:
+        sleep(random.randrange(0, 2))
+        random_row = random.randint(0, 11000)
+        engine = new_connector.create_db_connector()
+
+        with engine.connect() as connection:
+
+            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+            pin_selected_row = connection.execute(pin_string)
+            
+            for row in pin_selected_row:
+                pin_result = dict(row._mapping)
+
+            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_selected_row = connection.execute(geo_string)
+            
+            for row in geo_selected_row:
+                geo_result = dict(row._mapping)
+
+            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_selected_row = connection.execute(user_string)
+            
+            for row in user_selected_row:
+                user_result = dict(row._mapping)
+            
+            print(pin_result)
+            print(geo_result)
+            print(user_result)
+
+def send_data_to_api(endpoint, data):
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(endpoint, data=json.dumps(data), headers=headers)
+    if response.status_code == 200:
+        print(f"Data successfully sent to {endpoint}")
+    else:
+        print(f"Failed to send data {endpoint}. Status code: {response.status_code}, Response: {response.text}")
+        
+
+
+
+
+if __name__ == "__main__":
+    run_infinite_post_data_loop()
+    print('Working')
+    
+    
+
+
