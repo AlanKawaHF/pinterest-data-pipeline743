64999fe2540f8be05f4582777fbf0598e73e4afd
AlanKawaHF <70645103+AlanKawaHF@users.noreply.github.com>
2024-09-04 15:28:39 +0100

    script

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -15,7 +15,7 @@ random.seed(100)
 class AWSDBConnector:
 
     def __init__(self, creds_file="db_creds.yaml"):
-        
+
         with open(creds_file, "r") as file:
             creds = yaml.safe_load(file)
             self.HOST = creds["database"]["host"]
@@ -37,6 +37,14 @@ user_topic_endpoint = f"{API_URL}/12e8a20c3827.user"
 pin_topic_endpoint = f"{API_URL}/12e8a20c3827.pin"
 geo_topic_endpoint = f"{API_URL}/12e8a20c3827.geo"
 
+def send_data_to_api(endpoint, data):
+    headers = {"Content-Type": "application/vnd.kafka.json.v2+json"}
+    response = requests.post(endpoint, data=json.dumps(data), headers=headers)
+    if response.status_code == 200:
+        print(f"Data successfully sent to {endpoint}")
+    else:
+        print(f"Failed to send data {endpoint}. Status code: {response.status_code}, Response: {response.text}")
+
 def run_infinite_post_data_loop():
     while True:
         sleep(random.randrange(0, 2))
@@ -50,30 +58,30 @@ def run_infinite_post_data_loop():
             
             for row in pin_selected_row:
                 pin_result = dict(row._mapping)
+                print(f"Pin result: {pin_result}")
+                send_data_to_api(pin_topic_endpoint, pin_result)
 
             geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
             geo_selected_row = connection.execute(geo_string)
             
             for row in geo_selected_row:
                 geo_result = dict(row._mapping)
+                print(f"Geo result: {geo_result}")
+                send_data_to_api(geo_topic_endpoint, geo_result)
 
             user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
             user_selected_row = connection.execute(user_string)
             
             for row in user_selected_row:
                 user_result = dict(row._mapping)
+                print(f"User result: {user_result}")
+                send_data_to_api(user_topic_endpoint, user_result)
             
             print(pin_result)
             print(geo_result)
             print(user_result)
 
-def send_data_to_api(endpoint, data):
-    headers = {"Content-Type": "application/vnd.kafka.json.v2+json"}
-    response = requests.post(endpoint, data=json.dumps(data), headers=headers)
-    if response.status_code == 200:
-        print(f"Data successfully sent to {endpoint}")
-    else:
-        print(f"Failed to send data {endpoint}. Status code: {response.status_code}, Response: {response.text}")
+
         
 
 
